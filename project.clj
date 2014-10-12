bfd12e4bedf38768536ae25fab4e57b62916b521
Carsten Behring <carsten.behring@gmail.com>
2014-10-12 23:39:43 +0200

    added R protocoll implementation for rincanter
    -> "seq(1,10)" works for both and retuns the same

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [opencpu-clj "0.0.1-SNAPSHOT"]      ;for testing only
+                 [com.evocomputing/rincanter "1.0.0-SNAPSHOT"]
                  ])
--- src/r_clj/r.clj
+++ src/r_clj/r.clj
@@ -1,5 +1,6 @@
 (ns r-clj.r
-  (:require [opencpu-clj.ocpu :as ocpu :refer[call-R-function] ])
+  (:require [opencpu-clj.ocpu :as ocpu :refer[call-R-function]]
+            [com.evocomputing.rincanter :as rincanter :refer [r-eval]])
   )
 
 (defprotocol R
@@ -9,20 +10,27 @@
 (defrecord ocpu []
     R
   (R-call-function [_ execution-context package-name function-name parameters]
-    (ocpu/call-R-function (:server execution-context) package-name function-name parameters :json)))
+    (vec (ocpu/call-R-function (:server execution-context) package-name function-name parameters :json))))
 
 
 
-;implementation of protocol R via jvmr
-(defrecord jvmr []
+;implementation of protocol R via rincanter
+(defrecord rincanter []
            R
   (R-call-function [_ execution-context package-name function-name parameters]
-    (println "call to jvmr of function: " function-name)))
+    (r-eval (format "%s::%s(%s=%s,%s=%s)"
+                       package-name
+                       function-name
+                       (name (first (keys parameters)))
+                       (first (vals parameters))
+                       (name (second (keys parameters)))
+                       (second (vals parameters))))))
 
 
 
 
-;; define a function, which can be called by passing an implementor of the R protocoll
+
+    ;; define a function, which can be called by passing an implementor of the R protocoll
 (defn r-seq [ctx from to]
   (R-call-function (:r ctx) ctx "base" "seq" {:from from :to to}))
 
@@ -33,7 +41,7 @@
 
 (defn test-call []
   (let [R-ocpu (ocpu.)
-        R-jvmr (jvmr.)]
+        R-rincanter (rincanter.)]
     (do
       (println (r-seq {:r R-ocpu :server "http://public.opencpu.org"} 1 10))
-      (println (r-seq {:r R-jvmr :dont-know "something"} 1 10)))))
+      (println (r-seq {:r R-rincanter} 1 10)))))
