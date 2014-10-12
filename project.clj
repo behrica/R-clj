5bd69f3a6acb32c08c9fed55e90ad20db311fba9
Carsten Behring <carsten.behring@gmail.com>
2014-10-13 01:22:38 +0200

    added jvmr support

--- project.clj
+++ project.clj
@@ -5,5 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [opencpu-clj "0.0.1-SNAPSHOT"]      ;for testing only
-                 [com.evocomputing/rincanter "1.0.0-SNAPSHOT"]
-                 ])
+                 [com.evocomputing/rincanter "1.0.0-SNAPSHOT"] ;for testing only
+                 [org.ddahl/jvmr_2.11 "2.11.1.1"] ;for testing only
+                 ]
+  :repositories [["crealytics" "http://dl.bintray.com/content/crealytics/maven"]]
+
+  )
--- src/r_clj/r.clj
+++ src/r_clj/r.clj
@@ -1,7 +1,25 @@
 (ns r-clj.r
+  (:import (org.ddahl.jvmr RInJava))
   (:require [opencpu-clj.ocpu :as ocpu :refer[call-R-function]]
             [com.evocomputing.rincanter :as rincanter :refer [r-eval]])
   )
+
+
+
+(defn make-call-string [package-name function-name parameters]
+  (format "%s::%s(%s=%s,%s=%s)"
+          package-name
+          function-name
+          (name (first (keys parameters)))
+          (first (vals parameters))
+          (name (second (keys parameters)))
+          (second (vals parameters)))
+
+  )
+
+
+
+
 
 (defprotocol R
   (R-call-function [_ execution-context package-name function-name parameters]))
@@ -18,19 +36,20 @@
 (defrecord rincanter []
            R
   (R-call-function [_ execution-context package-name function-name parameters]
-    (r-eval (format "%s::%s(%s=%s,%s=%s)"
-                       package-name
-                       function-name
-                       (name (first (keys parameters)))
-                       (first (vals parameters))
-                       (name (second (keys parameters)))
-                       (second (vals parameters))))))
+    (r-eval (make-call-string package-name function-name parameters))))
+
+
+(defrecord jvmr []
+  R
+  (R-call-function [_ execution-context package-name function-name parameters]
+    (let [JVMR (RInJava.)]
+      (do
+        (.eval JVMR (format "result <- %s" (make-call-string package-name function-name parameters)))
+        (vec (.toVectorInt JVMR "result"))))))
 
 
 
-
-
-    ;; define a function, which can be called by passing an implementor of the R protocoll
+      ;; define a function, which can be called by passing an implementor of the R protocoll
 (defn r-seq [ctx from to]
   (R-call-function (:r ctx) ctx "base" "seq" {:from from :to to}))
 
@@ -41,7 +60,11 @@
 
 (defn test-call []
   (let [R-ocpu (ocpu.)
-        R-rincanter (rincanter.)]
+        R-rincanter (rincanter.)
+        R-jvmr (jvmr.)
+        ]
     (do
-      (println (r-seq {:r R-ocpu :server "http://public.opencpu.org"} 1 10))
-      (println (r-seq {:r R-rincanter} 1 10)))))
+      (println "ocpu (public): " (r-seq {:r R-ocpu :server "http://public.opencpu.org"} 1 10))
+      (println "ocpu (local):  " (r-seq {:r R-ocpu :server "http://localhost:6124"} 1 10))
+      (println "rincanter:     " (r-seq {:r R-rincanter} 1 10)))
+      (println "jvmr:          " (r-seq {:r R-jvmr} 1 10))))
